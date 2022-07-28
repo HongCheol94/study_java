070a3d826d12b0c467955b7f49051dc787ba852f
hongcheol94 <macotong3@naver.com>
2022-07-28 16:54:02 +0900

    .

---
+++ src/com/hongcheol/study/ch02/Hello.java
@@ -0,0 +1,46 @@
+package com.hongcheol.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+//		데이터 타입 변수명 = 값;
+//		데이터타입 변수명
+//		데이터타입 변수명 = 소개값
+		
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+
+		
+		String name ="김홍철";
+		
+		String carinfo = "51하1024";
+		String gear = "오토";
+		String carkind = "suv";
+		String color = "brown";
+		String fuel = "디젤";
+		int mileage = 70000;
+		
+		System.out.println(name);
+		System.out.println("name 변수에 들어가 있는 값은" + name);
+		
+		System.out.println("집에보내주세요"+name);
+		
+		System.out.println("차량번호가뭔가요?"+carinfo);
+		System.out.println("차량색상이뭔가요"+color);
+		System.out.println("내연기관이 뭔가요"+fuel);
+		System.out.println("변속기가어떻게되나요?"+gear);
+		System.out.println("주행거리가얼마나요?"+mileage);
+		System.out.println("차량종류가뭔가요"+carkind);
+		
+		
+		
+		
+		
+		
+				
+	}
+
+}
