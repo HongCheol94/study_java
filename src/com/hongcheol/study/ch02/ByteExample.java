f69ac6d80942ccfa17be45eef3615ff067056180
hongcheol94 <macotong3@naver.com>
2022-07-28 17:30:31 +0900

    java

---
+++ src/com/hongcheol/study/ch02/ByteExample.java
@@ -0,0 +1,27 @@
+package com.hongcheol.study.ch02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 123;
+//		byte var6 = 128; byte의 범위가 -128~127까지여서 128은 넘기때문에 오류가 난다
+		short var6 = 128;
+		
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+//		System.out.println(var6);
+		System.out.println(var6);
+
+	}
+
+}
