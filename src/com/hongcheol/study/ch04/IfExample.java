95678d418222f2f63b94f1ed01c854500e7b0153
hongcheol94 <macotong3@naver.com>
2022-08-08 17:39:49 +0900

    .

---
+++ src/com/hongcheol/study/ch04/IfExample.java
@@ -0,0 +1,31 @@
+package com.hongcheol.study.ch04;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		
+//		int score = 80;
+		
+//		if(score >= 90) {
+			System.out.println("A");
+//		} else {
+			System.out.println("A는 아니구나");
+//		}
+
+			
+		int score = 85;
+		if(score >= 97) {
+			System.out.println("A+");
+		} else if(score >= 92) {
+			System.out.println("A-");
+		} else {
+			System.out.println("A");
+		}
+			
+		
+	
+		
+	
+	}
+
+}
