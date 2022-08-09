d9af0fbe0e0347b9c86f021d2fae9657317573aa
hongcheol94 <macotong3@naver.com>
2022-08-09 13:55:25 +0900

    .

--- src/com/hongcheol/study/ch04/IfExample.java
+++ src/com/hongcheol/study/ch04/IfExample.java
@@ -13,14 +13,17 @@ public class IfExample {
 //		}
 
 			
-		int score = 85;
-		if(score >= 97) {
-			System.out.println("A+");
-		} else if(score >= 92) {
-			System.out.println("A-");
-		} else {
+		int score = 80;
+		if(score >= 90) 
 			System.out.println("A");
-		}
+		 else if(score >= 80 && score < 90) 
+			System.out.println("B");
+		 else if(score >= 70 && score < 80)
+			System.out.println("C");
+		 else if(score >= 60 && score < 70)
+			 System.out.println("D");
+		 else
+			 System.out.println("F");
 			
 		
 	
