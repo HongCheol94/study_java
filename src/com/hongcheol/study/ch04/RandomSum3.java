5555ad5a29448d260d4add8e63283a7199858509
hongcheol94 <macotong3@naver.com>
2022-08-16 17:32:30 +0900

    .

---
+++ src/com/hongcheol/study/ch04/RandomSum3.java
@@ -0,0 +1,17 @@
+package com.hongcheol.study.ch04;
+
+public class RandomSum3 {
+
+	public static void main(String[] args) {
+		
+		// 1 ~ 100 사이의 랜덤 정수 2개를 발생시켜
+		// 두 수 사이의 합을 구하라
+
+		
+		double num1 = 1.0;
+		double num2 = 100.0;
+		
+		
+	}
+
+}
