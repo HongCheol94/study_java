ea78d0cc733b81bf0a92fcd07bb4131280502de1
hongcheol94 <macotong3@naver.com>
2022-08-26 17:20:50 +0900

    ....

---
+++ src/com/hongcheol/study/ch07/third/Computer.java
@@ -0,0 +1,20 @@
+package com.hongcheol.study.ch07.third;
+
+public class Computer extends Calculator {
+
+
+
+	@Override
+	public double areaCircle(double r) {
+		System.out.println("computer 객체의 areaCircle 실행");
+		return Math.PI*r*r;
+
+	
+//	우클릭 소스 오버라이드 인프먼트 메소드 방법도 잇음
+		
+		
+	
+	
+	
+	}
+}
