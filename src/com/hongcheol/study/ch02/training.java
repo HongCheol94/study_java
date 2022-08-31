e890f8564a26ec62b148145da4fd58c1a7efc563
hongcheol94 <macotong3@naver.com>
2022-08-31 09:49:16 +0900

    .

--- src/com/hongcheol/study/ch02/training.java
+++ src/com/hongcheol/study/ch02/training.java
@@ -1,15 +1,23 @@
 package com.hongcheol.study.ch02;
 
-public class training {
+import java.util.Scanner;
+
+public class training{
 
 	public static void main(String[] args) {
 		
-		int sum = 0;
+	//체스 총 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개 총 = 16개
 		
-		for(int i=0; i<=10; i++) {
-			System.out.println(i);
-			sum += 1;
+		Scanner sc = new Scanner(System.in);
+		int[]chess = {1,1,2,2,2,8};
+		int[]a = new int[chess.length];
+		
+		for(int i=0; i<chess.length; i++) {
+			int num = sc.nextInt();
+			a[i] = chess[i] - num;
 		}
-		System.out.println("sum: "+ sum);
+		for(int i=0; i<a.length; i++) {
+			System.out.println(a[i]+"");
+		}
 	}
 }
